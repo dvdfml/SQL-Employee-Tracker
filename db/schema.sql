29b61aa176f1684281ed94b4be28bf44d1f575ea
David Miranda <dvdfermiranda@yahoo.com.mx>
2024-06-17 11:26:25 -0600

    Added correct syntax and comments

--- db/schema.sql
+++ db/schema.sql
@@ -3,19 +3,22 @@ CREATE DATABASE employees_db;
 
 \c employees_db;
 
+-- Creating department table
 CREATE TABLE departments (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL,
-)
+    name VARCHAR(30) UNIQUE NOT NULL
+);
 
+-- Creating role table
 CREATE TABLE roles (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER,
     FOREIGN KEY (department_id) REFERENCES departments(id)
-)
+);
 
+-- Creating employee table
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
@@ -24,4 +27,4 @@ CREATE TABLE employees (
     manager_id INTEGER,
     FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES employees(id)
-)
+);
