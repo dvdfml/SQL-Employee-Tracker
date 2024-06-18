b731d75e8d19bc8df9808e1021f242b54cf52ace
David Miranda <dvdfermiranda@yahoo.com.mx>
2024-06-17 18:09:21 -0600

    Added 'View All' queries

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ const pool = new Pool(
 )
 
 const displayOptions = async () => {
-    let answer;
-    answer = await select({
+    const answer = await select({
         message: 'What would you like to do?',
         choices: [
             {
@@ -39,9 +38,44 @@ const displayOptions = async () => {
             },
             {
                 value: 'Add Department'
+            },
+            {
+                value: 'Update Department'
+            },
+            {
+                value: 'Exit'
             }]
-    })
+    });
+    queryDatabase(answer);
+}
 
+async function queryDatabase(answer) {
+    let result = {};
+    switch (answer) {
+        case 'View All Employees':
+            result = await pool.query(
+                `SELECT e.id, e.first_name, e.last_name, title, departments.name AS department, salary, m.first_name || ' ' || m.last_name AS manager
+                 FROM employees AS e
+                 INNER JOIN roles ON e.role_id = roles.id
+                 INNER JOIN departments ON roles.department_id = departments.id
+                 LEFT JOIN employees AS m ON e.manager_id = m.id`);
+            console.table(result.rows);
+            return displayOptions();
+
+        case 'View All Roles':
+            result = await pool.query(`SELECT roles.id, title, name AS department, salary FROM roles 
+                INNER JOIN departments ON roles.department_id = departments.id`);
+            console.table(result.rows);
+            return displayOptions();
+
+        case 'View All Departments':
+            result = await pool.query(`SELECT * FROM departments`);
+            console.table(result.rows);
+            return displayOptions();
+
+        case 'Exit':
+            return console.log('Goodbye!');
+    }
 }
 
 pool.connect();
